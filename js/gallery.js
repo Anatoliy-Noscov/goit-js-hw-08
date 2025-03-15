c8baa9ad27a9f6884f763872bab7b56101ae0b8c
Anatoliy_Noscov <svodnuy7777@gmail.com>
2025-03-15 18:12:44 +0200

    fin

--- js/gallery.js
+++ js/gallery.js
@@ -1,70 +1,71 @@
 const images = [
   {
     preview:
-      "https://cdn.pixabay.com/photo/ 2019/05/14/16/43 /rchids- 420282 0__480.jpg",
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__340.jpg",
     original:
-      "https://cdn.pixabay.com/photo/ 2019/05/14/16/43 /rchids- 420282 0_ 1280 .jpg",
-    описание: "Hokkaido Flower",
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
+    description: "Hokkaido Flower",
   },
   {
     preview:
-      "https://cdn.pixabay.com/photo/ 2019/05/14/22/05 /container- 420367 7__340.jpg",
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
     original:
-      "https://cdn.pixabay.com/photo/ 2019/05/14/22/05 /container- 420367 7_ 1280 .jpg",
-    описание: "Container Haulage Freight",
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+    description: "Container Haulage Freight",
   },
   {
     preview:
-      "https://cdn.pixabay.com/photo/ 2019/05/16/09/47 /beach- 420678 5__340.jpg",
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
     original:
-      "https://cdn.pixabay.com/photo/ 2019/05/16/09/47 /beach- 420678 5_ 1280 .jpg",
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
     description: "Aerial Beach View",
   },
   {
     preview:
-      "https://cdn.pixabay.com/photo/ 2016/11/18/16/19 /flowers- 183561 9__340.jpg",
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
     original:
-      "https://cdn.pixabay.com/photo/ 2016/11/18/16/19 /flowers- 183561 9_ 1280 .jpg",
-    описание: "Flower Blooms",
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+    description: "Flower Blooms",
   },
   {
     preview:
-      "https://cdn.pixabay.com/photo/ 2018/09/13/10/36 /mountains- 367433 4__340.jpg",
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
     original:
-      "https://cdn.pixabay.com/photo/ 2018/09/13/10/36 /mountains- 367433 4_ 1280 .jpg",
-    описание: "Alpine Mountains",
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
+    description: "Alpine Mountains",
   },
   {
     preview:
-      "https://cdn.pixabay.com/photo/ 2019/05/16/23/04 /landscape- 420857 1__340.jpg",
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
     original:
-      "https://cdn.pixabay.com/photo/ 2019/05/16/23/04 /landscape- 420857 1_ 1280 .jpg",
-    описание: "Mountain Lake Sailing",
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
+    description: "Mountain Lake Sailing",
   },
   {
     preview:
-      "https://cdn.pixabay.com/photo/ 2019/05/17/09/27 /the-alps- 420927 2__340.jpg",
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
     original:
-      "https://cdn.pixabay.com/photo/ 2019/05/17/09/27 /the-alps- 420927 2_ 1280 .jpg",
-    описание: "Alpine Spring Meadows",
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    description: "Alpine Spring Meadows",
   },
   {
     preview:
-      "https://cdn.pixabay.com/photo/ 2019/05/16/21/10 /landscape- 420825 5__340.jpg",
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
     original:
-      "https://cdn.pixabay.com/photo/ 2019/05/16/21/10 /landscape- 420825 5_ 1280 .jpg",
-    описание: "Nature Landscape",
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    description: "Nature Landscape",
   },
   {
     preview:
-      "https://cdn.pixabay.com/photo/ 2019/05/17/04/35 /lighthouse- 420884 3__340.jpg",
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
     original:
-      "https://cdn.pixabay.com/photo/ 2019/05/17/04/35 /lighthouse- 420884 3_ 1280 .jpg",
-    описание: "Lighthouse Coast Sea",
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    description: "Lighthouse Coast Sea",
   },
 ];
 
 const galleryContainer = document.querySelector(".gallery");
+// console.log(galleryContainer);
 
 galleryContainer.innerHTML = images
   .map(
@@ -77,6 +78,8 @@ galleryContainer.innerHTML = images
   )
   .join("");
 
+// console.log(galleryContainer.innerHTML);
+
 galleryContainer.addEventListener("click", (event) => {
   event.preventDefault();
 
@@ -88,30 +91,4 @@ galleryContainer.addEventListener("click", (event) => {
   `);
 
   instance.show();
-});
-
-const gallery = document.querySelector(".gallery");
-
-gallery.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  const img = event.target.closest(".gallery-image");
-  if (!img) return;
-
-  const modal = document.createElement("div");
-  modal.classList.add("modal");
-  modal.innerHTML = `<img src="${img.dataset.source}" alt="${img.alt}">`;
-  document.body.appendChild(modal);
-
-  setTimeout(() => modal.classList.add("show"), 10);
-
-  modal.addEventListener("click", () => {
-    const modalImg = modal.querySelector("img");
-    modalImg.classList.add("fade-out");
-
-    setTimeout(() => {
-      modal.classList.remove("show");
-      setTimeout(() => modal.remove(), 300);
-    }, 200);
-  });
 });
